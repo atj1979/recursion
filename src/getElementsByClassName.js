ad3ef541f957cf981aedf454f126d74db089a025
Anthony Johnson <atj1979@gmail.com>
2015-01-23 18:29:03 -0800

    Completed getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,43 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var jim = function(className){
-
+var getElementsByClassName = function(className){
+  //count scanned elements
   var result=[];
-  var run= false;
-  var numrun = 0 ;// taverse the document
-		//get document topnode document.
-		//if child, get child
-		//if no child, get sibling,
-		//if no sibling get parent sibling
-		//if no parent sibling done
-  
   var recursion = function (domThing){
-    run=true;
     //get all class names per element
     //use that array and compare with target className
     //if an element matches push to our results
-    var classList = domThing.classList;
-    for (var thing in classList){
-      if (classList[thing] === className) {
-        console.log("pushed "+ className+ "" + domThing)
-        result.push(domThing);
-        // break here to account for duplicate class names
-        break;
+    if (domThing.classList!==undefined && domThing.classList!==null){
+      var classList = domThing.classList;
+      for (var thing in classList){
+        if (classList[thing] == className) {
+          result.push(domThing);
+          // break here to account for duplicate class names
+          // no items added more than once
+          break;
+        }
       }
     }
+    //taverse the document
+    //assume the sent node has no parents(top most) that need to be searched
+    //get document topnode document.
+    //if child, work on the child
+    //if sibling, work on the sibling
     if (domThing.hasChildNodes()){
       recursion(domThing.firstChild);
-    } else if (domThing.nextSibling) {
+    } 
+    if (domThing.nextSibling !== null) {
       recursion(domThing.nextSibling);
-
-    } else if (domThing.parentNode.nextSibling) {
-      recursion(domThing.parentNode.nextSibling);
     }
   };
   recursion(document.documentElement);
-  console.log("total " +result.length);
 	return result;
-	//push found search to result
-
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,52 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
-
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  if (obj===false){return String(obj);}
+  if (obj===true){return String(obj);}
+  if (obj===null){return String(null);}
+  if (obj===undefined){return undefined;}
+  if (typeof(obj)==="number"){return String(obj);}
+  if (typeof(obj)==="string"){return ('"'+ obj+ '"');} 
+  //array 
+  if (typeof(obj)==="object" && obj.length !== undefined){
+     var result;
+     if (obj.length===0){
+      return '[]'
+     } else {
+      for (var i =0; i< obj.length; i++){
+        if (i===0) {
+          result = stringifyJSON(obj[i]);
+        } else {
+          result += ',' + stringifyJSON(obj[i]);
+        }
+      }
+      return '['+result+']';
+    }
+  }
+  //object 
+  if (typeof(obj)==="object" && obj.length === undefined){
+    var result;
+    var first= true;
+    //if an empty object
+    if (Object.getOwnPropertyNames(obj).length===0){
+      return "{}";
+    } else {
+    for (var key in obj){
+      if (first){
+          //function consideration in an object
+          if (typeof(obj)==="function" || typeof(obj[key])==="function"){
+            return '{}';
+          }
+        result = stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+        first = false;
+      } else {
+        result += ',' + stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+      }
+    }}
+    return '{' + result + '}'; 
+  }
 };
+
